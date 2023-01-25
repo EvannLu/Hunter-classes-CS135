ac3762368447489741935262ca5be6cd16b17b60
Evan Lu <evan.l60@myhunter.cuny.edu>
2023-01-24 19:57:56 -0500

    week zero assignment completed

--- weekzero/60.cpp
+++ weekzero/60.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 using namespace std;
- 
-int main() {
-    cout << "Hello World!" << endl << "C++ is fun!" << endl;
-    return 0;
+
+int main(){
+    int n;
+    cout << "Give a whole number between -128 and 127: " << endl;
+    cin >> n;
+
+    int num = n;
+    if (num < 0){
+        num = -num-1;
+    }
+
+    string result;
+    int rem;
+    while (num > 0){
+        rem = num%2;
+        result = to_string(rem) + result; 
+        num /=2;
+    }
+
+    int size = 8;
+    int len = result.length();
+    for (int i=0; i < size-len; i++){
+        result = "0" + result;
+    }
+
+    if (n<0){
+        for (int i = 0; i < result.length(); i++){
+            if (result[i] == '0'){
+                result[i] = '1';
+            }
+            else if (result[i] == '1'){
+            result[i] = '0';
+            }
+        }
+    }
+    cout << "Binary string: " << result << endl;
 }
