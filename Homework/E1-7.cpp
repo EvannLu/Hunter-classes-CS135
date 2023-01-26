2032a9694d4a4bf3947b31cc14aa3bb12b078b81
Evan Lu <evan.l60@myhunter.cuny.edu>
2023-01-25 21:03:53 -0500

    done

---
+++ Homework/E1-7.cpp
@@ -0,0 +1,21 @@
+/*
+Author: Evan Lu
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Homework E1.7 - Three Items
+
+Description: A program that prints three items on three separate lines.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main(){
+    string a, b, c;
+    a = "car"; 
+    b = "truck"; 
+    c = "boat"; 
+    cout << a << endl << b << endl << c << endl;
+    return 0; 
+}
